70669e48d150adcf8584c52486cfaeeee4a3ae0a
PerryFinn <c913910173@163.com>
2025-03-14 16:09:17 +0800

    chore(changeset): 更新变更集提交逻辑
    - 修改了获取版本信息的逻辑，以支持单个发布的情况
    - 更新了 lint 脚本，添加了对未匹配错误的处理
    - 新增了变更集提交的映射文件

--- scripts/changeset.commit.ts
+++ scripts/changeset.commit.ts
@@ -19,6 +19,14 @@ const getVersionMessage: CommitFunctions["getVersionMessage"] = async (
   const publishableReleases = releasePlan.releases.filter((release) => release.type !== "none");
   const numPackagesReleased = publishableReleases.length;
 
+  if (publishableReleases.length === 1) {
+    const release = publishableReleases[0];
+    if (!release) {
+      throw new Error("release info is undefined");
+    }
+    return `chore(release): ${release.name}@${release.newVersion}`;
+  }
+
   const releasesLines = publishableReleases.map((release) => `  ${release.name}@${release.newVersion}`).join("\n");
 
   return `chore(release): releasing ${numPackagesReleased} package(s)
