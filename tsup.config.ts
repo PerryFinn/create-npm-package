304c47eb4a403577aa83e7196dd1e949257b1b73
PerryFinn <perryfinn@163.com>
2025-05-20 23:57:41 +0800

    chore: 更新 tsup 配置
    - 更新 tsup 配置，添加对 changeset.commit.ts 的支持，调整输出目录和格式设置

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entryPoints: {
-    index: "src/index.ts"
-    // 重新生成 changeset.commit.cjs 文件时需要
-    // "changeset.commit": "./scripts/changeset.commit.ts"
+export default defineConfig([
+  {
+    entry: {
+      index: "src/index.ts"
+    },
+    format: ["cjs", "esm", "iife"],
+    dts: true,
+    sourcemap: true,
+    outDir: "dist",
+    clean: true,
+    minify: false
   },
-  format: ["cjs", "esm", "iife"],
-  dts: true,
-  sourcemap: true,
-  outDir: "dist",
-  clean: true
-});
+  {
+    entry: {
+      // 重新生成 changeset.commit.cjs 文件时需要
+      "changeset.commit": "./scripts/changeset.commit.ts"
+    },
+    format: ["cjs"],
+    sourcemap: true,
+    outDir: ".changeset",
+    clean: false
+  }
+]);
