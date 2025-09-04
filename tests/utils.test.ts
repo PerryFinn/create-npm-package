11a7fe49f9d77536ae77419b129da93560842796
PerryFinn <perryfinn@163.com>
2025-09-04 19:23:57 +0800

    chore: 迁移测试文件

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { add } from "./utils";
+import { add } from "../src/utils";
 
 describe("add", () => {
   it("should return the sum of two numbers", () => {
