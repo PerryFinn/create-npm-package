0484ae973ee5bd5ebcc14061c5e0b6210845f2b8
PerryFinn <perryfinn@163.com>
2025-10-13 20:51:03 +0800

    chore: 添加占位测试集

--- tests/core/index.test.ts
+++ tests/core/index.test.ts
@@ -1,5 +1,8 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("core", () => {
   // TODO: Add tests for core
+  it("这里需要一个占位测试，防止报错", () => {
+    expect(true).toBe(true);
+  });
 });
